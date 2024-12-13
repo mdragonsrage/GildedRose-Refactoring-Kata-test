8524ffe7650810e414c149a926e59493b19f25d2
dragonsrage <max.cassiani@gmail.com>
2024-12-13 19:38:50 +0100

    removed unused languages

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -14,4 +14,12 @@ public class GildedRoseTest
         app.UpdateQuality();
         Assert.Equal("foo", Items[0].Name);
     }
+
+    public static IEnumerable<object[]> BaseItems()
+    {
+        yield return new object[] { new Item { Name = "foo", SellIn = 0, Quality = 0 },  };
+    }
+    
+    [Theory]
+    [MemberData(nameof)]
 }

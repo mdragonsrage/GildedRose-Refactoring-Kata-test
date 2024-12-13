a55d32b36e81b9a36fcecf282cbbc9b684161571
dragonsrage <max.cassiani@gmail.com>
2024-12-13 21:50:31 +0100

    extracted default behaviour, switch case for readability, update for Aged Brie and normal items

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -4,14 +4,34 @@ namespace GildedRoseKata;
 
 public class GildedRose
 {
-    private readonly IList<Item> Items;
+    private readonly IList<Item> _items;
 
     public GildedRose(IList<Item> items)
     {
-        this.Items = items;
+        _items = items;
     }
 
     private static void UpdateQualityForItem(Item item)
+    {
+        string name = item.Name;
+        switch (name)
+        {
+            case "Aged Brie":
+                UpdateBrie(item);
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert": 
+            case "Sulfuras, Hand of Ragnaros":
+                    DefaultMethod(item);
+                break;
+            default:
+                UpdateCommon(item);
+                break;
+        };
+        
+        //DefaultMethod(item);
+    }
+
+    private static void DefaultMethod(Item item)
     {
         if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
         {
@@ -50,10 +70,7 @@ public class GildedRose
             }
         }
 
-        if (item.Name != "Sulfuras, Hand of Ragnaros")
-        {
-            item.SellIn = item.SellIn - 1;
-        }
+        UpdateSellIn(item);
 
         if (item.SellIn < 0)
         {
@@ -83,10 +100,41 @@ public class GildedRose
             }
         }
     }
-    
+
+    private static void UpdateSellIn(Item item)
+    {
+        if (item.Name != "Sulfuras, Hand of Ragnaros")
+        {
+            item.SellIn -= 1;
+        }
+    }
+
+    private static void UpdateCommon(Item item)
+    {
+        UpdateSellIn(item);
+        if (item.Quality <= 0) return;
+        item.Quality -= 1;
+        if (item.SellIn < 0 && item.Quality > 0)
+        {
+            item.Quality -= 1;
+        }
+    }
+
+    private static void UpdateBrie(Item item)
+    {
+        UpdateSellIn(item);
+        if (item.Quality >= 50) return;
+        item.Quality += 1;
+        if (item.SellIn < 0)
+        {
+            item.Quality += 1;
+        }
+
+    }
+
     public void UpdateQuality()
     {
-        foreach (var item in Items)
+        foreach (var item in _items)
         {
             UpdateQualityForItem(item);
         }
